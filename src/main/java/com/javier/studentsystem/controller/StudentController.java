a2441359614903d0b0a16ac7dba0e70c47d30713
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-11-23 13:08:23 -0800

    add get mapping

--- src/main/java/com/javier/studentsystem/controller/StudentController.java
+++ src/main/java/com/javier/studentsystem/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.javier.studentsystem.controller;
 import com.javier.studentsystem.model.Student;
 import com.javier.studentsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -17,6 +16,11 @@ public class StudentController {
     @PostMapping("/add")
     public String add(@RequestBody Student student){
         studentService.saveStudent(student);
-        return "New student is added";
+        return "New student has been added";
     }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    };
 }
--- src/main/java/com/javier/studentsystem/service/StudentService.java
+++ src/main/java/com/javier/studentsystem/service/StudentService.java
@@ -2,6 +2,9 @@ package com.javier.studentsystem.service;
 
 import com.javier.studentsystem.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 }
